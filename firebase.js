5f9d963fe609c6eaf12fa31f4917ff3f571a853a
Felipe <fynatsumi@gmail.com>
2025-05-16 17:51:17 -0300

    continhas

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Sua configuração do Firebase (substitua pelas suas credenciais)
 const firebaseConfig = {
-  apiKey: "AIzaSyC_X9lEDqfGhuC06anFDj_lVMedr2I4LBY",
-  authDomain: "continhas-f48d1.firebaseapp.com",
-  projectId: "continhas-f48d1",
-  storageBucket: "continhas-f48d1.firebasestorage.app",
-  messagingSenderId: "897214134148",
-  appId: "1:897214134148:web:ddb98c1daaae5476cb4fd1",
-  measurementId: "G-6QDFDN9QQN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inicializa o Firebase
